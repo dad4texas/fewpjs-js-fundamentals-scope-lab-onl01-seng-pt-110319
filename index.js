5c3250300f688345a9c1d1a0347794ddecc8d7e3
Warren Harper II <warrenharper2@att.net>
2020-06-02 16:41:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function add2(n) {
 
 
 var funkyFunction = function() {
-  return function() {	  return function() {
-    return "FUNKY!"	    return "FUNKY!"
+  return function() {
+    return "FUNKY!"
+    var theFunk = funkyFunction
+    var theFunk = funkyFunction()()
